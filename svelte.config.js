7928d4a54f6cda99a7f2c6530c13d9ca777c6241
notozeki <notozeki@gmail.com>
2023-03-11 15:43:44 +0900

    Add prerender configs

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,17 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		prerender: {
+			entries: [
+				'/products/svelte-book',
+				'/products/react-book',
+				'/products/vue-book',
+				'/products/angular-book'
+			],
+			origin: 'https://svelte-book-sample-app.vercel.app'
+		}
 	}
 };
 
