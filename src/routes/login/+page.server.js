6db05c79e449d335cff556d9d4baea51513dc386
notozeki <notozeki@gmail.com>
2023-03-11 13:57:29 +0900

    Migrate `invalid` to `fail`

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,4 +1,4 @@
-import { invalid } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
 import { sendPasswordlessLink } from '$lib/server/auth0';
 
 export const actions = {
@@ -7,12 +7,12 @@ export const actions = {
 		const email = data.get('email');
 
 		if (!email) {
-			return invalid(400, { email, error: 'missing' });
+			return fail(400, { email, error: 'missing' });
 		}
 		// 正しいメールアドレスのフォーマットであるかを検査します.
 		// この正規表現は簡易版です。実際の業務では必要に応じてより正確な正規表現を使用しましょう。
 		if (!/^.+@.+$/.test(email)) {
-			return invalid(400, { email, error: 'invalid_format' });
+			return fail(400, { email, error: 'invalid_format' });
 		}
 
 		const state = crypto.randomUUID();
